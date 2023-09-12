200ab6155a8035957514f4a9eb430b01ea9bb298
kishan-11 <kishankumar.patel@housing.com>
2023-09-12 22:07:20 +0530

    food app

--- App.js
+++ App.js
@@ -1,6 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello world from React");
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src="https://penji.co/wp-content/uploads/2022/08/10.-mr.-d-food-logo.jpg" alt="logo" className="logo"/>
+      </div>
+      <div className="nav-container">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <h3>Meghna Foods</h3>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  )
+}
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />)
